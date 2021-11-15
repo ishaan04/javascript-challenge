f042dae93d1538a2422d565e7662530dd7ba4775
ishaan04 <ishaan_nigam01@yahoo.com.au>
2021-11-15 18:41:41 +1100

    push3

--- UFO-Level-1/Static/js/app.js
+++ UFO-Level-1/Static/js/app.js
@@ -2,18 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//data.js and index.html predefined, style.css added  different color
 
-//Setup reference to the table data nodes
+
+///SETUP REFERENCE TO THE TABLE DATA NODES
 var datatable=d3.select("tbody")
 
 //console.log(data)
-
+//table setup
 var row=datatable.append("tr")
 tableData.forEach(function(d) {
   var row = datatable.append("tr");
 
-    Object.entries(d).forEach(function([key, value]) {
-            var celldata = row.append("td");
+    Object.entries(d).forEach(function([key, value]) 
+        {var celldata = row.append("td");
             //console.log(data)
             celldata.text(value);
           });
@@ -22,25 +24,30 @@ tableData.forEach(function(d) {
 ///SETUP FILTERS/// 
 
 //SETUP BUTTON, retrieve inputed date and filter the table based on it
-var button=d3.select("#filter-btn");
-var search=d3.select("#datetime")
+var button=d3.select("#filter-btn"); //linked with the index.html
+var search=d3.select("#datetime") //linked with index.html
+
+//Setup the filtering function below
 function filterdata()
     {
-      console.log("filter button clicked")
+      console.log("user clicked button") //from class activity
       var nextDate = search.property("value");
       console.log(nextDate);
-      var tabledatanew=tableData.filter(t=>t.datetime==nextDate);
-      let tablebody = d3.select("tbody");
+      
+      var tabledatanew=tableData.filter(t=>t.datetime==nextDate); //scan the table
+      let tablebody = d3.select("tbody"); //linked with html
       tablebody.html("");
+      
       tabledatanew.forEach(function(d1) {
         console.log(d1);
         ///loop through each row and append table
         var row = tablebody.append("tr");
-          Object.entries(d1).forEach(function([key, value]) {
-
-                  var cell = row.append("td");
+          Object.entries(d1).forEach(function([key, value]) 
+          {var cell = row.append("td");
                   cell.text(value);
                 });
               });
     }
+
+// Activite the button
 button.on("click",filterdata);
